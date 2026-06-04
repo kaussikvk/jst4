c210a6b66642b4616f8797e5c5e4aa5947d21e36
Kaussik <kaussikvk@gmail.com>
2026-06-04 17:39:17 +0530

    redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,77 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import TaskFour from "./TaskFour";
+import TaskFive from "./TaskFive";
+import TaskSix from "./TaskSix";
+import TaskSeven from "./TaskSeven";
+import TaskThirteen from "./TaskThirteen";
+import TaskFourteen from "./TaskFourteen";
 import TaskFifteen from "./TaskFifteen";
 
 function App() {
-  return <TaskFifteen />;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav className="navbar">
+          <Link to="/task4">Task4</Link>
+
+          <Link to="/task5">Task5</Link>
+
+          <Link to="/task6">Task6</Link>
+
+          <Link to="/task7">Task7</Link>
+
+          <Link to="/task13">Task13</Link>
+
+          <Link to="/task14">Task14</Link>
+
+          <Link to="/task15">Task15</Link>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/task4"
+            element={<TaskFour />}
+          />
+
+          <Route
+            path="/task5"
+            element={<TaskFive />}
+          />
+
+          <Route
+            path="/task6"
+            element={<TaskSix />}
+          />
+
+          <Route
+            path="/task7"
+            element={<TaskSeven />}
+          />
+
+          <Route
+            path="/task13"
+            element={<TaskThirteen />}
+          />
+
+          <Route
+            path="/task14"
+            element={<TaskFourteen />}
+          />
+
+          <Route
+            path="/task15"
+            element={<TaskFifteen />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
