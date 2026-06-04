5c07ddfbfb0e178c924716088627ef034ae717ab
Kaussik <kaussikvk@gmail.com>
2026-06-04 17:50:07 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,82 @@
 import {
-  BrowserRouter,
   Routes,
   Route,
   Link,
 } from "react-router-dom";
 
 import TaskFour from "./TaskFour";
+import TaskFive from "./TaskFive";
 import TaskSix from "./TaskSix";
 import TaskSeven from "./TaskSeven";
 import TaskThirteen from "./TaskThirteen";
 import TaskFourteen from "./TaskFourteen";
 import TaskFifteen from "./TaskFifteen";
 
+import "./App.css";
+
 function App() {
   return (
-      <div>
-        <nav className="navbar">
-          <Link to="/task4">Task4</Link>
+    <div>
+      <nav className="navbar">
+        <Link to="/">Home</Link>
 
+        <Link to="/task4">Task4</Link>
 
-          <Link to="/task6">Task6</Link>
+        <Link to="/task5">Task5</Link>
 
-          <Link to="/task7">Task7</Link>
+        <Link to="/task6">Task6</Link>
 
-          <Link to="/task13">Task13</Link>
+        <Link to="/task7">Task7</Link>
 
-          <Link to="/task14">Task14</Link>
+        <Link to="/task13">Task13</Link>
 
-          <Link to="/task15">Task15</Link>
-        </nav>
+        <Link to="/task14">Task14</Link>
 
-        <Routes>
-          <Route
-            path="/task4"
-            element={<TaskFour />}
-          />
+        <Link to="/task15">Task15</Link>
+      </nav>
 
-        
+      <Routes>
+        <Route
+          path="/"
+          element={<TaskFour />}
+        />
 
-          <Route
-            path="/task6"
-            element={<TaskSix />}
-          />
+        <Route
+          path="/task4"
+          element={<TaskFour />}
+        />
 
-          <Route
-            path="/task7"
-            element={<TaskSeven />}
-          />
+        <Route
+          path="/task5"
+          element={<TaskFive />}
+        />
 
-          <Route
-            path="/task13"
-            element={<TaskThirteen />}
-          />
+        <Route
+          path="/task6"
+          element={<TaskSix />}
+        />
 
-          <Route
-            path="/task14"
-            element={<TaskFourteen />}
-          />
+        <Route
+          path="/task7"
+          element={<TaskSeven />}
+        />
 
-          <Route
-            path="/task15"
-            element={<TaskFifteen />}
-          />
-        </Routes>
-      </div>
-  
+        <Route
+          path="/task13"
+          element={<TaskThirteen />}
+        />
+
+        <Route
+          path="/task14"
+          element={<TaskFourteen />}
+        />
+
+        <Route
+          path="/task15"
+          element={<TaskFifteen />}
+        />
+      </Routes>
+    </div>
   );
 }
 
